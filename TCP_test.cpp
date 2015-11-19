6ee20ae587c923fed46d4676a8da78c35d15e96a
yuni-net <yuni.net.liberty@gmail.com>
2015-11-19 18:35:01 +0900

    failed to load message

--- TCP_test.cpp
+++ TCP_test.cpp
@@ -27,8 +27,9 @@ void Receiver::receive_line(SOCKET & sock)
 	int total_bytes = 0;
 	while (true)
 	{
-		total_bytes += recv(sock, buffer, buff_bytes, 0);
+		total_bytes += recv(sock, buffer, buff_bytes-1, 0);
 		if (total_bytes < 2) continue;
+
 		if (buffer[total_bytes - 2] != '\r') continue;
 		if (buffer[total_bytes - 1] != '\n') continue;
 		break;
@@ -47,40 +48,29 @@ const char * Receiver::get_text() const
 	return buffer;
 }
 
-
-bool get_status(SOCKET & sock)
+class MailDownloader
 {
-	Receiver receiver;
-
-	char * user = "USER nhs30070";
-	printf("%s\n", user);
-	char command_user[1024];
-	sprintf(command_user, "%s%s", user, "\r\n");
-	send(sock, command_user, strlen(command_user), 0);
-
-	receiver.receive_line(sock);
-	receiver.show();
-
-	char * pass = "PASS d19941005";
-	printf("%s\n", pass);
-	char command_pass[1024];
-	sprintf(command_pass, "%s%s", pass, "\r\n");
-	send(sock, command_pass, strlen(command_pass), 0);
-
-	receiver.receive_line(sock);
-	receiver.show();
-
-	puts("user name and password is ok.\n");
-	// todo
-	return true;
-}
+public:
+	bool connect_to_server();
+	int get_mail_quantity() const;
+	void download_show_mail(int index);
+	void finish();
 
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	Receiver receiver;
-
+	~MailDownloader();
+private:
 	SOCKET sock;
+	int mail_quantity;
+
+	bool get_status();
+};
+
+
+
+bool MailDownloader::connect_to_server()
+{
+	Receiver receiver;
+
 	SOCKADDR_IN ServAddr;
 	u_short ServPort = 110;
 	char servIP[256] = "192.168.120.11";
@@ -107,13 +97,98 @@ int _tmain(int argc, _TCHAR* argv[])
 		sizeof(ServAddr)) == SOCKET_ERROR) {
 		printf("接続エラー\n");
 		getchar();
-		return -1;
+		return false;
 	}
 
 	receiver.receive_line(sock);
 	receiver.show();
 
-	get_status(sock);
+	get_status();
+
+
+}
+
+int MailDownloader::get_mail_quantity() const
+{
+	return mail_quantity;
+}
+
+void MailDownloader::download_show_mail(int index)
+{
+	char request[1024];
+	sprintf(request, "RETR %u\r\n", index);
+	send(sock, request, strlen(request), 0);
+
+	Receiver receiver;
+	receiver.receive_line(sock);
+	receiver.show();
+	const char * reply = receiver.get_text();
+	if (memcmp(reply, "+OK", 3) != 0)
+	{
+		puts("メッセージの受信に失敗しました。\n");
+		return;
+	}
+
+	while (true)
+	{
+		receiver.receive_line(sock);
+		receiver.show();
+		const char * text = receiver.get_text();
+		const int length = strlen(text);
+		const char * end3 = text + length - 3;
+		if (memcmp(end3, ".\r\n", 3) == 0)
+		{
+			return;
+		}
+	}
+}
+
+void MailDownloader::finish()
+{
+	char * quit = "QUIT";
+	printf("%s\n", quit);
+	char command_quit[1024];
+	sprintf(command_quit, "%s%s", quit, "\r\n");
+	send(sock, command_quit, strlen(command_quit), 0);
+
+	Receiver receiver;
+	receiver.receive_line(sock);
+	receiver.show();
+}
+
+MailDownloader::~MailDownloader()
+{
+	shutdown(sock, SD_BOTH);
+	closesocket(sock);
+
+	WSACleanup();
+}
+
+
+
+bool MailDownloader::get_status()
+{
+	Receiver receiver;
+
+	char * user = "USER nhs30070";
+	printf("%s\n", user);
+	char command_user[1024];
+	sprintf(command_user, "%s%s", user, "\r\n");
+	send(sock, command_user, strlen(command_user), 0);
+
+	receiver.receive_line(sock);
+	receiver.show();
+
+	char * pass = "PASS d19941005";
+	printf("%s\n", pass);
+	char command_pass[1024];
+	sprintf(command_pass, "%s%s", pass, "\r\n");
+	send(sock, command_pass, strlen(command_pass), 0);
+
+	receiver.receive_line(sock);
+	receiver.show();
+
+	puts("user name and password is ok.\n");
 
 	char * stat = "STAT";
 	printf("%s\n", stat);
@@ -124,22 +199,32 @@ int _tmain(int argc, _TCHAR* argv[])
 	receiver.receive_line(sock);
 	receiver.show();
 
-	char * quit = "QUIT";
-	printf("%s\n", quit);
-	char command_quit[1024];
-	sprintf(command_quit, "%s%s", quit, "\r\n");
-	send(sock, command_quit, strlen(command_quit), 0);
+	int damy;
+	sscanf(receiver.get_text(), "+OK %d %d", &mail_quantity, &damy);
 
-	receiver.receive_line(sock);
-	receiver.show();
-
-	shutdown(sock, SD_BOTH);
-	closesocket(sock);
-
-	WSACleanup();
-
-	getchar();
-
-	return 0;
+	return true;
 }
 
+
+/// nhs30070@nagoya.hal.ac.jp
+
+
+int _tmain(int argc, _TCHAR* argv [])
+{
+	MailDownloader loader;
+	loader.connect_to_server();
+
+	int index;
+	if (loader.get_mail_quantity() > 0)
+	{
+		printf("メールが%d件届いています。\n", loader.get_mail_quantity());
+		puts("何番目のメールを受信しますか？\n");
+		printf("0～%dの間で指定してください。\n", loader.get_mail_quantity() - 1);
+		scanf("%d", &index);
+	}
+
+	loader.download_show_mail(index);
+	loader.finish();
+
+	getchar();
+}
